c95845969f9098a5807fb4abf51260eadd372042
Monfernape <engr.usmankhalil@gmail.com>
2024-05-06 07:53:41 +0500

    fix: added fetch filter for current month only

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -6,7 +6,31 @@ import { DatabaseTable } from "@/utils/supabase/client";
 
 const Expenses = async () => {
   const supabaseClient = createClient();
-  const { data } = await supabaseClient.from(DatabaseTable.Expenses).select().returns<Expense[]>();
+  var currentDate = new Date();
+
+  // Get the first day of the current month
+  var startDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  ).toISOString();
+
+  // Get the last day of the current month
+  var endDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  ).toISOString();
+
+  console.log({ startDate, endDate })
+
+  const { data } = await supabaseClient
+    .from(DatabaseTable.Expenses)
+    .select()
+    .order("created_at", { ascending: false })
+    .gt("created_at", startDate)
+    .lt("created_at", endDate)
+    .returns<Expense[]>();
   return <ExpensesList expenses={data ?? []} />;
 };
 
