ce66330c0fc6b74dbaa6766b87579bd9b5344fe3
Monfernape <engr.usmankhalil@gmail.com>
2024-11-14 11:32:01 +0500

    feat: redirect by default to expenses page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/expenses",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
